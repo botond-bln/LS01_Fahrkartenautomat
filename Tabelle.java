339003b17d64068b828a7a5a0edf5c84e7a9f695
botond-bln <botond.vr@gmail.com>
2022-12-05 11:20:32 +0100

    in arbeit

---
+++ Tabelle.java
@@ -0,0 +1,20 @@
+public class Tabelle {
+    
+    public static void main(String[] args) {
+
+    String f = "Fahrenheit";
+    String c = "Celsius";
+    String linie = "----------------------";
+
+    System.out.printf("%s                     ", f, c);
+    
+
+    
+
+
+
+
+
+
+    }
+}
