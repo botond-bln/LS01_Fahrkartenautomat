94f0bcaadba7e1240320ceecc50e61ed2f2668c2
botond-bln <botond.vr@gmail.com>
2022-12-05 11:52:15 +0100

    fertig

--- Tabelle.java
+++ Tabelle.java
@@ -5,16 +5,13 @@ public class Tabelle {
     String f = "Fahrenheit";
     String c = "Celsius";
     String linie = "----------------------";
+    String strich = "|";
+    
 
-    System.out.printf("%s                     ", f, c);
+    System.out.printf(" %s  |%9s \n %s \n -20 \t %5s \t -28.89 \n +10 \t %5s \t -23.33 \n +0 \t %5s \t -17.78 \n +20 \t %5s \t  -6.67 \n +30 \t %5s \t  -1.11", f, c, linie, strich, strich, strich, strich, strich);
     
 
     
-
-
-
-
-
 
     }
 }
